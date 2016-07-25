135c3ad2aed02c5779a963bc2a984e3abbaf5b80
goliczk <goliczk@gmail.com>
2016-07-25 16:14:44 -0400

    Delete controller.js

--- controller.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-var app = angular.module('madLibsApp', []);
-
-app.controller('inputController', function($scope, madLibsFactory){
-	$scope.ourFunction = function(madLib) {
-		madLibsFactory.saveObject(madLib);
-	};
-});
